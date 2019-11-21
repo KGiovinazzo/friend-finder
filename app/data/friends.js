57d547932e310f8c4526724875095fb2ea407573
Kaitlyn Giovinazzo <kgiovinazzo42@gmail.com>
2019-11-20 21:21:47 -0500

    Adding HTML

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+var friends = [
+    {
+        name: "Bob Belcher",
+        photo: "",
+        scores: [1, 5, 3, 5, 1, 4, 4, 5, 3, 3]
+    },
+    {
+        name: "Linda Belcher",
+        photo: "",
+        scores: [4, 2, 4, 2, 5, 1, 2, 3, 2, 4]
+    },
+    {
+        name: "Tina Belcher",
+        photo: "",
+        scores: [2, 4, 2, 4, 2, 5, 3, 4, 5, 2]
+    },
+    {
+        name: "Gene Belcher",
+        photo: "",
+        scores: [3, 3, 5, 3, 4, 3, 5, 2, 4, 1]
+    },
+    {
+        name: "Louise Belcher",
+        photo: "",
+        scores: [5, 1, 1, 1, 3, 2, 1, 1, 1, 5]
+    }    
+]
---
+++ server.js
@@ -0,0 +1,15 @@
+//Dependencies
+var express = require("express");
+var path = require("path");
+
+//Setting up express 
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+//Router
+require('.app/routing/apiRoutes')(app);
+require('.app/routing/htmlRoutes')(app);
+
+app.listen(PORT, function() {
+    console.log("App is listening on PORT: " + PORT);
+});
