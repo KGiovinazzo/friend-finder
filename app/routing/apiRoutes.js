1f13e2f7602fc647eca769c48e59a50f0dc62420
Kaitlyn Giovinazzo <kgiovinazzo42@gmail.com>
2019-12-03 20:52:59 -0500

    updated api and html routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,43 @@
 var friends = require("./app/data/friends");
 
-console.log(friends);
+// console.log(friends);
+
+module.exports = function(app) {
+    //GET requests
+    app.get("/api/friends", function(req, res){
+        res.json(friends);
+    });
+
+    //POST request
+    //Compatability logic
+    app.post("/api/friends", function(req, res) {
+        var newFriend = req.body;
+
+        //Loop through score to make array
+
+        for (var i = 0; i < newFriend.scores.length; i++){
+            newFriend.scores[i] = parseInt(newFriend.scores[i]);
+        };
+
+        var newBestie = 0;
+
+        var scoreDifference = 40;
+
+        for (var j = 0; j <friends.length; j++) {
+            var friendScore = 0;
+
+            for (var k = 0; k < friends[j].scores.length; k++) {
+                var comparedScore = Math.abs(newFriend.scores[k] - friends[j].scores[k]);
+                friendScore += comparedScore;
+            };
+        }
+    })
+
+
+
+
+
+}
 
 app.get("./api/:friends", function(req, res) {
 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,6 +3,14 @@ var path = require("path");
 module.export = function(app) {
 //Survey route
 
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(_dirname, "../public/survey.html"));
+});
+
 //if no matching route, default to home
 
-}
+app.get("*", function(req, res) {
+    res.sendFile(path.join(_dirname, "../public/home.html"));
+});
+
+};
